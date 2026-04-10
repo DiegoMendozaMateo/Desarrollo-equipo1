2fdeab7ade76f734fab29f2160abbeb189f49f3e
Guadalupe0405 <hemg050704@gs.utm.mx>
2026-04-10 14:44:18 -0600

    Add Google Fonts link to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,16 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+          rel="stylesheet"
+        />
+      </head>
+
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
